96b042104709e5033ac5166ee09d0435f005b008
xvxii <xvxii@BAONGUYEN-PC>
2020-05-07 09:52:43 +0700

    Initial commit

---
+++ src/main/java/com/baonguyen/demo/SpringMongodbReactjsApplication.java
@@ -0,0 +1,13 @@
+package com.baonguyen.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMongodbReactjsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMongodbReactjsApplication.class, args);
+	}
+
+}
